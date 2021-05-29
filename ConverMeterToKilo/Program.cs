204ef58398efaad38d103e91497597da6bbbb169
Stilyan <sbaklarev@gmail.com>
2021-05-29 14:37:17 +0300

    new exercise

---
+++ ConverMeterToKilo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConverMeterToKilo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double meters = double.Parse(Console.ReadLine());
+
+            double result = meters * 0.100;
+            sdfdas\
+            Console.WriteLine($"{result:f2}");
+        }
+    }
+}
---
+++ PoundsToDollar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PoundsToDollar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
